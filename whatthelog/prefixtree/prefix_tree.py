dceac57d166a861ac7f1b4c95fa0640567e550f3
Tommaso Brandirali <tommaso.brandirali@gmail.com>
2021-05-03 19:45:31 +0200

    Fix imports for prefix tree

--- whatthelog/prefixtree/prefix_tree.py
+++ whatthelog/prefixtree/prefix_tree.py
@@ -13,7 +13,7 @@ from typing import List, Union
 # Internal
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-from prefixtree.edge_properties import EdgeProperties
+from whatthelog.prefixtree.edge_properties import EdgeProperties
 from whatthelog.prefixtree.graph import Graph
 from whatthelog.prefixtree.state import State
 from whatthelog.exceptions import InvalidTreeException
